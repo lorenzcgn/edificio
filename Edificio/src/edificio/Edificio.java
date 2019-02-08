b6c8f966d61aea9e142f8efe95b5513a3447c950
Lorenzo Cagni <45255725+lorenzocagni@users.noreply.github.com>
2019-02-08 11:49:49 +0100

    Update Edificio.java

--- Edificio/src/edificio/Edificio.java
+++ Edificio/src/edificio/Edificio.java
@@ -1,5 +1,6 @@
 package edificio;
 import java.util.*;
+import java.lang.*;
 
 
 /**
@@ -11,41 +12,50 @@ public class Edificio {
 
     public static void main(String[] args) {
     int numeroPiani=7;
+    int numProp[];
+    numProp = new int[14];
+
     
     Scanner s = new Scanner(System.in);
     
     /* Chiedo il numero dei piani */
     
-    System.out.println("Inserisci il numero di piani massimo dell'edificio (un piano corrisponde ad un appartamento): ");
+    System.out.println("Inserisci il numero di piani (un piano corrisponde ad un appartamento): ");
     do{
         numeroPiani=s.nextInt();
-    }while(numeroPiani<1);    
+    }while(numeroPiani<1);
+    
     
     /* Array di oggetti */
     
     Appartamento Appartamenti[]= new Appartamento[numeroPiani];
-    Proprietario Proprietari[]= new Proprietario[numeroPiani];
-
     
-    /* Chiedo i dati del proprietario e degli appartamenti */
+    
+    for (int i=0;i<numeroPiani;i++) {
+    System.out.println("Inserisci il numero dei proprietari (max 2) degli appartamenti: ");
+    do{
+        numProp[i]=s.nextInt();
+    }while(numProp[i]<1 || numProp[i]>2);
+   
+    
+    }
+    
+    /* Chiedo i dati del proprietario e instanzio gli appartamenti */
     
     for (int i=0; i<numeroPiani; i++) {
         Appartamenti[i]= new Appartamento(0,0,0,7,0,0,0,0);
-        Proprietari[i]= new Proprietario("","","");
     }
-    
-    Appartamento a1 = new Appartamento(1,1,1,7,0,0,0,0);
-    Appartamento a2 = new Appartamento(1,1,1,7,0,0,0,0);
-    
-    for (int i=0; i<numeroPiani; i++) {
-        Appartamenti[i].chiediDatiAppartamento();
-        Proprietari[i].chiediDatiProp();
-    }
-    
+
     int volumi=0;
     int superfici=0;
     int disperdenteLorda=0;
     int disperdenteNetta=0;
+    
+    Appartamento a1 = new Appartamento(1,numProp[0],1,7,0,0,0,0);
+    Appartamento a2 = new Appartamento(2,numProp[1],2,7,0,0,0,0);
+    
+    a1.settaProprietari(numProp[0]);
+    a2.settaProprietari(numProp[1]);
        
     Stanza A = new Stanza("Stanza1", 30, 10, 10, 10, true, false);
     Stanza B = new Stanza("Stanza2", 20, 10, 10, 10, true, false);
@@ -58,19 +68,29 @@ public class Edificio {
     
     
     for (int i=0; i<1; i++) {
-            volumi+=a1.getVol();
-            superfici+=a1.getSup();
-            disperdenteLorda+=a1.getSupDisp();
-            disperdenteNetta+=a1.getSupDispN();
+            volumi+=a1.getVol()+a2.getVol();
+            superfici+=a1.getSup()+a2.getSup();
+            disperdenteLorda+=a1.getSupDisp()+a2.getSupDisp();
+            disperdenteNetta+=a1.getSupDispN()+a2.getSupDispN();
     }
     
     System.out.println(
                 "\n"+
+                "Di seguito ecco i dati dell'intero edificio: "+
     		"Volumi totali: "+volumi+"\n"+
     		"Superfici totali: "+superfici+"\n"+
     		"Superfici disperdenti lorde: "+disperdenteLorda+"\n"+
     		"Superfici disperdenti nette: "+disperdenteNetta
     );
+    
+    System.out.println("Vuoi anche sapere chi sono i proprietari degli appartamenti? Scrivi SI o NO");
+    String scelta;
+    scelta=s.next();
+    if (scelta.equals("SI")){
+        System.out.println("L'appartamento al piano "+a1.getNumPiano()+", con "+ a1.getNumeroStanze()+" stanze, il cui numero interno e' "+a1.getNumInterno()+" ha "+a1.getNumProp()+"\n"+
+                " proprietari: "+a1.P1.getNome());
+    }
+    else System.out.println("Ok, grazie per aver usato questo programma");
     
 
 }}
